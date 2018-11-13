76678a671825404ff2394c1f6ef352f0df28854e
Menachem Polonsky <m@mbenleonardo.com>
2018-11-13 16:34:56 -0500

    blocJamsSliders-Assignment and Bonus Completed

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
-    var totalTime $(song.duration)
-    $('#time-control .total-time').text(totalTime);
+    const totalTime = player.getDuration();
+    $('#time-control .total-time').text(player.prettyTime(totalTime));
   };
 }
 
